85c6da26df681cf223ec7a74fd92cc1421b2823a
Ajay88888 <crxajay@gmail.com>
2021-01-15 12:37:26 +0530

    small

---
+++ SmallNo.java
@@ -0,0 +1,15 @@
+package com.rays.basic;
+
+public class SmallNo {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		if (a < b) {
+			System.out.println("small no is = " + a);
+		} else {
+			System.out.println("small no is = " + b);
+
+		}
+	}
+}
